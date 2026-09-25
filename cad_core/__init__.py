48f93b224e7a4bb9a46cc519b9aa622a13a0b6c2
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-25 15:09:21 -0400

    Expose electromechanical component model

--- cad_core/__init__.py
+++ cad_core/__init__.py
@@ -8,3 +8,5 @@ from .geometry_adapter import CADGeometry
 __all__ = ["CADState","StateKind","StateStore","OptimizationProblem","GradientDescentOptimizer","SimulationRun","CADMath","CADGeometry"]
 
 from .electromechanical import Connection, ThreePhaseResult, solve_three_phase, winding_graph
+
+from .electromechanical_components import ComponentKind, ElectromechanicalComponent, WindingComponent, MachineAssembly
