aa874472babf51995f31d8ec8d6ea4a41ce5cc7d
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-25 15:39:13 -0400

    Allow cache-busted relative viewer asset URL

--- tests/test_viewer_pages_assets.py
+++ tests/test_viewer_pages_assets.py
@@ -3,7 +3,7 @@ H=(Path(__file__).resolve().parents[1]/"ung-cad-3d"/"viewer.html").read_text()
 
 def test_viewer_assets_exist_at_referenced_repo_relative_paths():
     assert 'src="./csg-engine.js"' in H
-    assert 'src="./viewer.js"' in H
+    assert 'src="./viewer.js' in H
     assert 'src="./ung-geometry-integration.js"' in H
     root=Path(__file__).resolve().parents[1]/"ung-cad-3d"
     assert (root/"csg-engine.js").is_file()
