3e4face01233859dcff7affc511af79b1ed8e9bf
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-25 14:45:56 -0400

    Fix malformed geometry integrity regression test

--- tests/test_geometry_integrity_ui.py
+++ tests/test_geometry_integrity_ui.py
@@ -13,5 +13,6 @@ def test_geometry_integrity_engine_is_wired():
         assert token in JS
 
 def test_integrity_gate_is_refreshed_with_scene():
-    assert "updateTwin(one)" in JS\n    assert "updateIntegrity()" in JS
+    assert "updateTwin(one)" in JS
+    assert "updateIntegrity()" in JS
     assert "integrity-run" in JS
